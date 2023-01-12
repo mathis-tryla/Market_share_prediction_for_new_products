ac17721cb59c987173476571ac1b04f562627fb5
Warrior62 <50228030+Warrior62@users.noreply.github.com>
2023-01-12 23:23:11 +0100

    Take into account zero-sales-number dataframe

--- shampoos_seasonality_multiplier-4.py
+++ shampoos_seasonality_multiplier-4.py
@@ -1,126 +1,188 @@
-# -*- coding: utf-8 -*-
-
+import matplotlib.pyplot as plt
+import sys, time
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
 import pandas as pd
-from io import StringIO, BytesIO
-import math, time, os, sys 
-from matplotlib import pyplot as plt
-from datetime import datetime
-from tqdm import tqdm
+from io import StringIO
+from scipy.special import logsumexp
 
-def get_sales_per_timestamp(dataframe):
-  dict_sales_numbers = {}
-  sales_numbers_list = []
-  for timestamp in dataframe['timestamp'].unique():
-    sales_numbers = dataframe.loc[dataframe['timestamp'] == timestamp, ['timestamp', 'barcode', 'sales_number']].sales_number.values.sum()
-    sales_numbers_list.append(sales_numbers)
-  dict_sales_numbers['timestamp'] = dataframe['timestamp'].unique()
-  dict_sales_numbers['sales_numbers'] = sales_numbers_list
-  return pd.DataFrame(dict_sales_numbers)
+def create_dataframe():
+  headers = ['timestamp','week','barcodes','type','segment','category','description','weight','sales_numbers','price','sales_value','discounts']
+  return pd.read_csv(sys.argv[1], sep=';', names=headers, index_col=False, encoding="utf-8", encoding_errors="ignore")
 
-def get_date_from_timestamp(timestamp):
-  date = datetime.fromtimestamp(timestamp)
-  whole_date = {}
-  whole_date["year"] = date.isocalendar()[0]
-  whole_date["week"] = date.isocalendar()[1]
-  whole_date["weekday"] = date.isocalendar()[2]
-  return whole_date
+def sort_data_per_barcode(df):
+  return df.sort_values('barcodes')
 
-def get_sales_per_season(dataframe):
-  # Spring = 20/03-21/06 = 11-25
-  spring = range(12, 26)
-  # Summer = 21/06-23/09 = 26-38
-  summer = range(26, 39)
-  # Fall = 23/09-21/12 = 39-51
-  fall = range(39, 52)
-  # Winter = 21/12-20/03 = 52-11
-  winter = range(0, 12) or range(52, 53)
-  spring_sales_numbers = dataframe.loc[(dataframe['week'] >= 12) & (dataframe['week'] <= 25), ['week', 'sales_numbers']].sales_numbers.values
-  summer_sales_numbers = dataframe.loc[(dataframe['week'] >= 26) & (dataframe['week'] <= 38), ['week', 'sales_numbers']].sales_numbers.values
-  fall_sales_numbers = dataframe.loc[(dataframe['week'] >= 39) & (dataframe['week'] <= 51), ['week', 'sales_numbers']].sales_numbers.values
-  winter_sales_numbers = dataframe.loc[(dataframe['week'] == 52) | ((dataframe['week'] >= 1) & (dataframe['week'] <= 11)), ['week', 'sales_numbers']].sales_numbers.values
-  return {"spring": spring_sales_numbers, "summer": summer_sales_numbers, "fall": fall_sales_numbers, "winter": winter_sales_numbers}
+def find_five_products(df):
+  five_products_barcodes = []
+  for barcode in df.barcodes.unique():
+    five_products_barcodes.append(barcode)
+  return five_products_barcodes[:5]
 
-# ventes moyennes de shampoings par semaine
-def get_average_sales_per_week(df):
-  if ('week' and 'sales_number') in df:
-    return df.groupby('week')["sales_number"].mean()
+def find_unique_weeks(df):
+  return df["week"].unique()
 
-# ventes moyenne de shampoings sur la période
-def get_average_sales_per_season(df):
-  # Get sales numbers per timestamp
-  df_sales_numbers = get_sales_per_timestamp(df)
-  df_sales_numbers = df_sales_numbers.sort_values('timestamp')
+def get_sales_numbers_for_five_shampoos(df, five_products_barcodes, week):
+  five_products_dict = {}
+  timestamps = []
+  categories = []
+  descriptions = []
+  sales_numbers = []
 
-  # Create a dataframe for sales numbers with year, week and weekday per timestamp
-  years = []
-  weeks = []
-  weekdays = []
-  for ts in df_sales_numbers['timestamp']:
-    timestamp = int(ts)
-    years.append(get_date_from_timestamp(timestamp)['year'])
-    weeks.append(get_date_from_timestamp(timestamp)['week'])
-    weekdays.append(get_date_from_timestamp(timestamp)['weekday'])
-  df_sales_numbers['year'] = years
-  df_sales_numbers['week'] = weeks
-  df_sales_numbers['weekday'] = weekdays
-  #
-  season_sales = 0
-  sales_per_season = get_sales_per_season(df_sales_numbers)
-  average_sales_per_season = {}
-  for season in sales_per_season:
-    for sale in sales_per_season[season]:
-      season_sales += sale
-    if len(sales_per_season[season]) == 0:
-      average_sales_per_season[season] = 0
-    else:
-      average_sales_per_season[season] = (season_sales/len(sales_per_season[season]))
-  return pd.DataFrame(average_sales_per_season, index=[0])
+  for barcode in five_products_barcodes:
+    sales_numbers_arr = df.loc[(df['barcodes'] == barcode) & (df['week'] == week), ['barcodes','sales_numbers']].sales_numbers.values
+    sales_number = 0
+    for sn in sales_numbers_arr:
+      sales_number += sn
+    sales_numbers.append(sales_number)
 
-if __name__ == '__main__':
-  # Start chrono
-  start_time = time.time()
+  five_products_dict["barcodes"] = five_products_barcodes
+  five_products_dict["barcodes"].append("barcode new product")
+  five_products_dict["sales_numbers"] = sales_numbers 
+  five_products_dict["sales_numbers"].append(None)
+  return pd.DataFrame(five_products_dict)
 
-  """ sys.argv[]
-  1 : script which removes bad line in the dataset
-  2 : dataset file containing shampoos data
-  3 : 
-      0 is average shampoo sales per week
-      1 is average shampoo sales over the seasons
-  """
-  #try:
-  if len(sys.argv) > 1:
-    remove_bad_lines_file = sys.argv[1] 
-    dataset = sys.argv[2]
-    #average_shampoo_sales_filter = sys.argv[3]
+def get_competing_products_mshares(dataframe):
+  total_sales_number = dataframe['sales_numbers'].sum()
+  market_shares_list = [] 
+  print(f"total_sales_number={total_sales_number}")
+  if total_sales_number > 0:  
+    for sn in dataframe['sales_numbers']:
+      market_share = (sn/logsumexp(total_sales_number)) * 100
+      #print(f"market_share={market_share}")
+      market_shares_list.append(round(market_share, 1))
+  dataframe['market_shares'] = market_shares_list
+  return dataframe
 
-    os.system(f"./{remove_bad_lines_file} {dataset}")
+def extract_adhoc():
+  rankingscores = []
+  worksheet = sys.argv[2] 
+  df = pd.read_excel(worksheet, sheet_name=0)
+  dfprods = []
+  #df.drop(df.head(1).index, inplace=True)
+  dfprod1 = df.iloc[:, 5]
+  dfprod2 = df.iloc[:, 6]
+  dfprod3 = df.iloc[:, 7]
+  dfprod4 = df.iloc[:, 8]
+  dfprod5 = df.iloc[:, 9]
+  dfprodTEST = df.iloc[:, 10]
 
-    # Create the dataframe
-    headers = ['timestamp','week','barcode','type','segment','category','description','weight','sales_number','price','sales_value','discounts']
-    df = pd.read_csv(dataset, sep=';', names=headers, index_col=False, encoding="utf-8", encoding_errors="ignore")
+  dfprods.append(dfprod1);dfprods.append(dfprod2);dfprods.append(dfprod3)
+  dfprods.append(dfprod4);dfprods.append(dfprod5);dfprods.append(dfprodTEST)
+  ranking_products = []
+  dict_ranking_products = {}
 
-    df_average_sales_per_week = get_average_sales_per_week(df)
-    #print(f"df_average_sales_per_week : {df_average_sales_per_week}")
+  for product in dfprods:
+    ranking_score_product = 0
+    # Get the number of occurences purcentage
+    # from 1 to 6
+    for number in range(1, product.nunique()+1):
+      purcentage = product.loc[product == number].value_counts() / len(product)
+      #score formula 
+      #if you have 7 products , from 7 points if you are first to 1 point if you are seventh
+      score = product.nunique() + 1 - number
+      ranking_score_number = purcentage*score
+      if len(ranking_score_number.values) > 0:
+        ranking_score_product += float(ranking_score_number)
+    ranking_products.append(ranking_score_product)
+  dict_ranking_products['ranking_scores'] = ranking_products
+  return pd.DataFrame(dict_ranking_products)
 
-    df_average_sales_per_season = get_average_sales_per_season(df)
-    #print(f"df_average_sales_per_season : {df_average_sales_per_season}")
+def eval_regression(degree, X, y, test_size, random_state):
+    poly = PolynomialFeatures(degree=degree, include_bias=False)
+    poly_variables = poly.fit_transform(X)
+    poly_var_train, poly_var_test, res_train, res_test = train_test_split(poly_variables, y, test_size = test_size, random_state = random_state)
+    regression = LinearRegression()
+    model = regression.fit(poly_var_train, res_train)
+    score = model.score(poly_var_test, res_test)
+    y_predicted = regression.predict(poly_variables)
+    rmse = mean_squared_error(y, y_predicted, squared=False)
+    return score,rmse,model,y_predicted
 
-    total_sales_numbers = df['sales_number'].sum()
+def get_convenient_regression_model(min_degree, max_degree, X, y, test_size, random_state):
+  convenient_degree = sys.maxsize
+  convenient_X = None
+  convenient_X_train = None
+  convenient_y_train = None
+  convenient_poly = None
+  convenient_y_predicted = None
+  min_rmse = sys.maxsize
+  min_model = ''
+  final_score = -1
+  if min_degree < max_degree:
+    for degree in range(min_degree, max_degree+1):
+      score,rmse,model,y_predicted = eval_regression(degree, X, y, test_size, random_state)   
+      if rmse < min_rmse:
+        convenient_degree = degree
+        min_rmse = rmse
+        min_model = model
+        convenient_y_predicted = y_predicted
+  return convenient_degree,min_rmse,min_model,convenient_y_predicted
 
-    # Calculate the part of sales numbers per season
-    print("\nCOEFF per week:")
-    for row in df_average_sales_per_week:
-      print(row/total_sales_numbers)
+def get_new_mshares(dataframe):  
+  #print("---- get_new_mshares")
+  # Predict the market share of the new market shares from the ranking scores 
+  #print(dataframe["ranking_scores"])
+  X, y = dataframe["ranking_scores"].iloc[:-1].values.reshape(-1,1), dataframe["market_shares"].iloc[:-1]
+  x=X[:,0]
 
-    print("\nCOEFF per season:")
-    print(f"spring={(df_average_sales_per_season['spring']/total_sales_numbers).values}")
-    print(f"summer={(df_average_sales_per_season['summer']/total_sales_numbers).values}")
-    print(f"fall={(df_average_sales_per_season['fall']/total_sales_numbers).values}")
-    print(f"winter={(df_average_sales_per_season['winter']/total_sales_numbers).values}")
+  if dataframe["ranking_scores"].iloc[:-1].isnull().values.any():
+    return pd.DataFrame()
+  else:
+    print(x)
+    #print(x.reshape(-1,1))
+    degree,rmse,model,y_predicted = get_convenient_regression_model(2, 20, x.reshape(-1,1), y, 0.3, 42)
+    #print(degree,rmse,model)
+    new_product_market_share = 100 - y_predicted.sum()
+
+    six_products_dict = {}
+    six_products_dict["barcodes"] = dataframe["barcodes"]
+    six_products_dict["sales_numbers"] = dataframe["sales_numbers"]
+    six_products_dict["ranking_scores"] = dataframe["ranking_scores"]
+    six_products_dict["old_market_shares"] = dataframe["market_shares"]
+    six_products_dict["new_market_shares"] = y_predicted
+    six_products_dict["new_market_shares"] = np.append(six_products_dict["new_market_shares"], new_product_market_share)
+    df_six_products = pd.DataFrame(six_products_dict)
+
+    # Predict the sales number of the new product from the new market shares 
+    X, y = df_six_products["new_market_shares"].iloc[:-1].values.reshape(-1, 1), df_six_products["sales_numbers"].iloc[:-1]
+    x=X[:,0]
+    degree,rmse,model,y_predicted = get_convenient_regression_model(2, 20, x.reshape(-1,1), y, 0.3, 42)
+
+    mymodel = np.poly1d(np.polyfit(x, y, degree))
+    new_product_market_share = df_six_products["new_market_shares"].iloc[-1]
+    estimated_new_product_sales_number = mymodel(new_product_market_share)
+
+    six_products_dict["new_sales_numbers"] = y_predicted
+    six_products_dict["new_sales_numbers"] = np.append(six_products_dict["new_sales_numbers"], estimated_new_product_sales_number)
+    return pd.DataFrame(six_products_dict)
+
+def main():
+  df = create_dataframe()
+  df = sort_data_per_barcode(df)
+  five_products_barcodes = find_five_products(df)
+  unique_weeks = find_unique_weeks(df)
+  df = df.dropna()
+  df_ranking_scores = extract_adhoc()
+  outputs_df = {}
+  for week in unique_weeks:
+      print(f"week={week}")
+      df_five_products = get_sales_numbers_for_five_shampoos(df, five_products_barcodes, week)
+      if df_five_products['sales_numbers'].sum() > 0:
+        df_five_products_mshares = get_competing_products_mshares(df_five_products)
+        df_five_products_mshares.insert(2, "ranking_scores", pd.Series(df_ranking_scores['ranking_scores'].values), allow_duplicates=True)
+        outputs_df[week] = get_new_mshares(df_five_products_mshares)
+      else:
+        outputs_df[week] = None
+      print(outputs_df[week]) 
+  return outputs_df
   
-  #except:
-  # End the chrono
-  end_time = time.time()
-  final_time = end_time - start_time
-  print(f"-- {final_time} seconds--")
-
+start_time = time.time()
+outputs_df = main()
+end_time = time.time()
+final_time = end_time-start_time
+print(f"-- {final_time} seconds --")
