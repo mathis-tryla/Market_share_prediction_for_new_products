97a3f142aec7cfe33de640140c810d9bc56e3bd3
Warrior62 <50228030+Warrior62@users.noreply.github.com>
2023-01-16 15:18:11 +0100

    Update remove_bad_lines_shampoo-2.sh

--- remove_bad_lines_shampoo-2.sh
+++ remove_bad_lines_shampoo-2.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 indexes=$(awk -F '[\t;]' 'NF-1!=12 {print NR}' $1)
-echo "indexes done"
-#cat $indexes
-#indexesArr=($indexes)
 read -a indexesArr <<< $indexes
-echo "indexesArr done"
 
 for index in "${indexesArr[@]}"
 do
     sed -i -e ''"${index}d"'' $1
 done
+echo "-- remove bad lines from dataset DONE"
