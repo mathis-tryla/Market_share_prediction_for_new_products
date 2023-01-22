5833c828f8cbaf7846a8324e15ee349f8a2946d7
Warrior62 <50228030+Warrior62@users.noreply.github.com>
2023-01-22 19:03:29 +0100

    Integration finished

--- calculate_population_knowing_the_product.py
+++ calculate_population_knowing_the_product.py
@@ -1,13 +1,10 @@
 # How to run this script :
-# 1: python3 calculate_population_knowing_the_product.py targeted_nb_of_contacts(int) reach(float) airings_per_week(float) length_of_campaign_in_week(int)
-# Ex : python3 calculate_population_knowing_the_product.py 3 0.25 2 10
+# 1: python3 calculate_population_knowing_the_product.py targeted_nb_of_contacts(int) reach(float) frequency(float)
+# Ex : python3 calculate_population_knowing_the_product.py 3 0.25 10
 
-import time
 import matplotlib.pyplot as plt
-import pandas
 from scipy import stats
-import sys
-
+import pandas as pd
 
 # Show the evolution of the population knowing the product
 def show_model_simulation(data):
@@ -21,20 +18,14 @@ def show_model_simulation(data):
     plt.legend(loc="lower right")
     plt.show()
 
-
 # Export the points to csv
 def export_to_csv(data):
-    df = pandas.DataFrame(data=data)
+    df = pd.DataFrame(data=data)
     df.set_index('week', inplace=True)
     df.to_csv("population_knowing_the_product.csv")
 
-
-def main():
-    if len(sys.argv) == 5:
-        targeted_nb_of_contacts = int(sys.argv[1])
-        reach = float(sys.argv[2])
-        frequency = float(sys.argv[3])
-        length_of_campaign = int(sys.argv[4])
+def get_population_knowing_product(targeted_nb_of_contacts, reach, frequency, length_of_campaign):
+    if targeted_nb_of_contacts is not None and reach is not None and frequency is not None and length_of_campaign is not None:
         if length_of_campaign > 26:
             length_of_campaign = 26
 
@@ -53,15 +44,5 @@ def main():
 
         show_model_simulation(data)
 
-        return "population_knowing_the_product.csv created"
-    else:
-        return "wrong number of arguments"
-
-
-start_time = time.time()
-output = main()
-end_time = time.time()
-final_time = end_time - start_time
-print(f"-- Calculate population knowing the product DONE")
-print(f"-- Output = {output} DONE")
-print(f"-- {final_time} seconds--")
+        return data
+    return None
